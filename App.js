8af79eaddf1cd137c1f6a1b771e477dc0784bcd5
vojone <vvd1@centrum.cz>
2022-12-03 16:16:28 +0100

    Created simple app for storing strings in async storage

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { store, get } from './store';
+
 
 export default function App() {
+  const [input, setInput] = useState('');
+
+  const onSave = () => {
+    if(!input) {
+      return;
+    }
+    else {
+      const objToStore = { text : input };
+      store('testKey', objToStore);
+
+      setInput('');
+    }
+  }
+
+  const onRetrieve = () => {
+    get('testKey').then(result => alert(result.text));
+  }
+
+  const inputChange = text => {
+    setInput(text);
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TextInput
+        type="text"
+        id="test"
+        name="test"
+        value={input}
+        placeholder="Type something"
+        onChangeText={inputChange}
+      />
+      <Button
+        title='Save'
+        onPress={onSave}
+      ></Button>
+      <Button
+        title='Retrieve'
+        onPress={onRetrieve}
+      ></Button>
     </View>
   );
 }
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # DiaDiary
 
 Mobile app for diabetics, that can be used for storing and analyzing data about hteir health.
+
+## Usage
+Testing and debugging (Expo Go appis needed):
+
+```
+npx expo run:android
+```
