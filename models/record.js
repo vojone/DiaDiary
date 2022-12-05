5570c06b8c513d4bd791b11e584769a1d3969390
vojone <vvd1@centrum.cz>
2022-12-05 20:19:50 +0100

    Fixed indentation

--- models/record.js
+++ models/record.js
@@ -2,52 +2,52 @@ import { store, get } from "../store";
 
 
 export class Record {
-    static storeKey = 'records';
+  static storeKey = 'records';
 
-    static async find(id) {
-        return get(`${Record.storeKey}.${id}`).then(result => {
-            if(result === null) {
-                return result;
-            } 
-            else {
-                return new Record({templateObj_ : result});
-            }
-        });
+  static async find(id) {
+    return get(`${Record.storeKey}.${id}`).then(result => {
+      if(result === null) {
+        return result;
+      } 
+      else {
+        return new Record({templateObj_ : result});
+      }
+    });
+  }
+
+  constructor({id_ = null, bloodSugar_ = null, templateObj_ = null} = {}) {
+    if(templateObj_) {
+      this.id = templateObj_.id;
+      this.bloodSugar = templateObj_.bloodSugar;
+    }
+    else {
+      this.id = id_;
+      this.bloodSugar = bloodSugar_;
+    }
+  }
+
+  obj() {
+    return {
+      'id' : this.id,
+      'bloodSugar' : this.bloodSugar, 
+    };
+  }
+
+  async save() {
+    toBeStored = this.obj();
+
+    if(toBeStored.id === null) {
+
+      let i = 0;
+      while(await Record.find(i) !== null) {
+        i++;
+      }
+      
+      toBeStored.id = i;
     }
 
-    constructor({id_ = null, bloodSugar_ = null, templateObj_ = null} = {}) {
-        if(templateObj_) {
-            this.id = templateObj_.id;
-            this.bloodSugar = templateObj_.bloodSugar;
-        }
-        else {
-            this.id = id_;
-            this.bloodSugar = bloodSugar_;
-        }
-    }
+    store(`${Record.storeKey}.${toBeStored.id}`, toBeStored);
 
-    obj() {
-        return {
-            'id' : this.id,
-            'bloodSugar' : this.bloodSugar, 
-        };
-    }
-
-    async save() {
-        toBeStored = this.obj();
-
-        if(toBeStored.id === null) {
-
-            let i = 0;
-            while(await Record.find(i) !== null) {
-                i++;
-            }
-            
-            toBeStored.id = i;
-        }
-
-        store(`${Record.storeKey}.${toBeStored.id}`, toBeStored);
-
-        return toBeStored.id;
-    }
+    return toBeStored.id;
+  }
 }
--- store.js
+++ store.js
@@ -2,48 +2,48 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 
 
 export async function store(key, data) {
-    try {
-        let jsonedData = JSON.stringify(data);
-        await AsyncStorage.setItem(`@${key}`, jsonedData);
-    }
-    catch (e) {
-        console.error(e);
-    }
+  try {
+    let jsonedData = JSON.stringify(data);
+    await AsyncStorage.setItem(`@${key}`, jsonedData);
+  }
+  catch (e) {
+    console.error(e);
+  }
 }
 
 export async function get(key) {
-    try {
-        let rawData = await AsyncStorage.getItem(`@${key}`);
-        if(rawData === null) {
-            console.log('Data with given key were not saved yet!');
-            AsyncStorage.getAllKeys().then(result => console.log(result));
-            return null;
-        }
-
-        let data = JSON.parse(rawData);
-
-        return data;
+  try {
+    let rawData = await AsyncStorage.getItem(`@${key}`);
+    if(rawData === null) {
+      console.log('Data with given key were not saved yet!');
+      AsyncStorage.getAllKeys().then(result => console.log(result));
+      return null;
     }
-    catch (e) {
-        console.error(e);
-    }
+
+    let data = JSON.parse(rawData);
+
+    return data;
+  }
+  catch (e) {
+    console.error(e);
+  }
 }
 
 export async function remove(key) {
-    try {
-        await AsyncStorage.removeItem(`@${key}`);
-    }
-    catch (e) {
-        console.error(e);
-    }
+  try {
+    await AsyncStorage.removeItem(`@${key}`);
+  }
+  catch (e) {
+    console.error(e);
+  }
 }
 
 
 export async function clear() {
-    try {
-        await AsyncStorage.clear();
-    }
-    catch (e) {
-        console.error(e);
-    }
+  try {
+    await AsyncStorage.clear();
+  }
+  catch (e) {
+    console.error(e);
+  }
 }
