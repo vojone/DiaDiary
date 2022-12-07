69c958e7fc8afeb37bf28ede14175040f5ef6fea
vojone <vvd1@centrum.cz>
2022-12-07 17:37:37 +0100

    fixed indentation in store.js file

--- services/store.js
+++ services/store.js
@@ -2,48 +2,48 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 
 
 export async function storeAS(key, data) {
-  try {
-    let jsonedData = JSON.stringify(data);
-    await AsyncStorage.setItem(`@${key}`, jsonedData);
-  }
-  catch (e) {
-    console.error(e);
-  }
+    try {
+        let jsonedData = JSON.stringify(data);
+        await AsyncStorage.setItem(`@${key}`, jsonedData);
+    }
+    catch (e) {
+        console.error(e);
+    }
 }
 
 export async function getAS(key) {
-  try {
-    let rawData = await AsyncStorage.getItem(`@${key}`);
-    if(rawData === null) {
-      console.log('Data with given key were not saved yet!');
-      AsyncStorage.getAllKeys().then(result => console.log(result));
-      return null;
+    try {
+        let rawData = await AsyncStorage.getItem(`@${key}`);
+        if(rawData === null) {
+            console.log('Data with given key were not saved yet!');
+            AsyncStorage.getAllKeys().then(result => console.log(result));
+            return null;
+        }
+
+        let data = JSON.parse(rawData);
+
+        return data;
     }
-
-    let data = JSON.parse(rawData);
-
-    return data;
-  }
-  catch (e) {
-    console.error(e);
-  }
+    catch (e) {
+        console.error(e);
+    }
 }
 
 export async function removeAS(key) {
-  try {
-    await AsyncStorage.removeItem(`@${key}`);
-  }
-  catch (e) {
-    console.error(e);
-  }
+    try {
+        await AsyncStorage.removeItem(`@${key}`);
+    }
+    catch (e) {
+        console.error(e);
+    }
 }
 
 
 export async function clearAS() {
-  try {
-    await AsyncStorage.clear();
-  }
-  catch (e) {
-    console.error(e);
-  }
+    try {
+        await AsyncStorage.clear();
+    }
+    catch (e) {
+        console.error(e);
+    }
 }
