36235b2c3c6f75bccece82e8b6e292ab2ff20983
vojone <vvd1@centrum.cz>
2022-12-07 20:21:06 +0100

    Added real items to bottom menu

--- App.js
+++ App.js
@@ -5,44 +5,54 @@ import Entypo from 'react-native-vector-icons/Entypo';
 
 import HomeScreen from './views/HomeScreen';
 import HistoryScreen from './views/HistoryScreen';
+import { View, Text, StyleSheet } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const styles = StyleSheet.create({
+      main: {
+          width: '100%',
+          height: '100%',
+      },
+  });
+
   return (
-    <NavigationContainer>
-      <Tab.Navigator initialRouteName='Home'>
-        <Tab.Screen
-            name='Nový záznam'
-            component={HomeScreen}
-            options={{ 
-                tabBarLabel: 'Přidat záznam',
-                tabBarIcon: ({color, size}) => (
-                    <MaterialCommunityIcons name="plus" color={color} size={size} /> 
+    <View style={styles.main}>
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName='Home'>
+          <Tab.Screen
+              name='Nový záznam'
+              component={HomeScreen}
+              options={{ 
+                  tabBarLabel: 'Přidat záznam',
+                  tabBarIcon: ({color, size}) => (
+                      <MaterialCommunityIcons name="plus" color={color} size={size} /> 
+                  ), 
+              }}
+          />
+          <Tab.Screen 
+              name='Graf' 
+              component={HistoryScreen}
+              options={{ 
+                  tabBarLabel: 'Graf',
+                  tabBarIcon: ({color, size}) => (
+                    <MaterialCommunityIcons name="chart-bell-curve-cumulative" color={color} size={size} /> 
                 ), 
-            }}
-        />
-        <Tab.Screen 
-            name='Graf' 
-            component={HistoryScreen}
-            options={{ 
-                tabBarLabel: 'Graf',
-                tabBarIcon: ({color, size}) => (
-                  <MaterialCommunityIcons name="chart-bell-curve-cumulative" color={color} size={size} /> 
-              ), 
-            }} 
-        />
-        <Tab.Screen 
-            name='Historie' 
-            component={HistoryScreen}
-            options={{ 
-                tabBarLabel: 'Histore',
-                tabBarIcon: ({color, size}) => (
-                  <Entypo name="back-in-time" color={color} size={size} /> 
-              ), 
-            }}  
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+              }} 
+          />
+          <Tab.Screen 
+              name='Historie' 
+              component={HistoryScreen}
+              options={{ 
+                  tabBarLabel: 'Histore',
+                  tabBarIcon: ({color, size}) => (
+                    <Entypo name="back-in-time" color={color} size={size} /> 
+                ), 
+              }}  
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </View>
   );
 }
