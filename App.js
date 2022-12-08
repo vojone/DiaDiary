69895b02e0c07a0c9e55ce3a7d283082c2f96f9f
Juraj Dedič <jurajdedic@gmail.com>
2022-12-08 14:50:40 +0100

    added basic diagram

--- App.js
+++ App.js
@@ -1,56 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { createBottomTabNavigator, createNativeStackNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 import HomeScreen from './views/HomeScreen';
 import HistoryScreen from './views/HistoryScreen';
 
-
-import { Button, View, Text, TouchableHighlight } from 'react-native';
-
-
 const Tab = createBottomTabNavigator();
 
-function MyTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} options={{
-        tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />),
-        headerRight: () => (
-          <TouchableHighlight onPress={() => alert('This is a button!')}>
-            <View style={{ width: 100, flexDirection: 'row' }}>
-              <Text style={{fontSize: 16}}>Settings</Text>
-              <MaterialCommunityIcons name="cog" size={21} color="black" style={{marginLeft: 4}}/>
-            </View>
-          </TouchableHighlight>
-        )}
-        }/>
-      <Tab.Screen name="History" component={HistoryScreen} options={{
-        tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="history" color={color} size={size} />
-          )}}/>
-    </Tab.Navigator>
-  );
-}
-
-const Stack = createNativeStackNavigator();
-
-
 export default function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Screen
-              name="Home"
-              component={HomeScreen}
-              options={{ title: 'Welcome' }}
-            />
-            <Stack.Screen name="History" component={HistoryScreen} />
-          <MyTabs />
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName='Home'>
+        <Tab.Screen
+            name='Nový záznam'
+            component={HomeScreen}
+            options={{ 
+                tabBarLabel: 'Přidat záznam',
+                tabBarIcon: ({color, size}) => (
+                    <MaterialCommunityIcons name="plus" color={color} size={size} /> 
+                ), 
+            }}
+        />
+        <Tab.Screen 
+            name='Graf' 
+            component={HistoryScreen}
+            options={{ 
+                tabBarLabel: 'Graf',
+                tabBarIcon: ({color, size}) => (
+                  <MaterialCommunityIcons name="chart-bell-curve-cumulative" color={color} size={size} /> 
+              ), 
+            }} 
+        />
+        <Tab.Screen 
+            name='Historie' 
+            component={HistoryScreen}
+            options={{ 
+                tabBarLabel: 'Histore',
+                tabBarIcon: ({color, size}) => (
+                  <Entypo name="back-in-time" color={color} size={size} /> 
+              ), 
+            }}  
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
