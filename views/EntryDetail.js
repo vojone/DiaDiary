cc67df2733a887b1086bf69783de03e17bf967a4
Tomáš Dvořák <xdvora3r@stud.fit.vutbr.cz>
2022-12-11 11:49:36 +0100

    Unfunctional reactive updates in view

--- views/EntryDetail.js
+++ views/EntryDetail.js
@@ -1,17 +1,80 @@
 import { StyleSheet, Text, View, TextInput, FlatList } from 'react-native';
-import EntryItem from '../components/EntryItem';
-import React, {useState} from 'react';
+import React, { useState, useImperativeHandle, useRef, useEffect } from 'react';
 import InputSpinner from "react-native-input-spinner";
 
 import { Button } from 'react-native-paper';
-import { primaryColor } from '../styles/common';
+import { addRecordStyles, bottomTabBarActiveBgColor, placeholderColor, primaryColor } from '../styles/common';
 import DateTimePicker from 'react-native-modal-datetime-picker';
 import ModalDropdown from 'react-native-modal-dropdown';
 import DateTimePickerWithText from '../components/DateTimePickerWithText';
+import AppendDropdown from '../components/AppendDropdown';
 
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Record } from '../models/record';
+import { Unit } from '../models/unit';
 
-export default function EntryDetail({ navigation }) {
+export default function EntryDetail({ route, navigation}) {
+  const {recordId} = route.params;
+
+  const [recordDetail, setRecordDetail] = useState({});
+  const [glycemiaUEnum, setGlycemiaUEnum] = useState([]);
+
+  Record.findById(recordId).then((record) => {console.log(record); setRecordDetail(record);});
+
+  
+  function updatedBloodSugar(value) {
+    setRecordDetail({
+      ...recordDetail,
+      bloodSugar: value.target.value
+    });
+  }
+
+  function updatedBloodSugarU(value) {
+    setRecordDetail({
+      ...recordDetail,
+      bloodSugarU: value.target.value
+    });
+  }
+
+  function updatedInsuline(value) {
+    setRecordDetail({
+      ...recordDetail,
+      insuline: value.target.value
+    });
+  }
+
+  function updatedInsulineT(value) {
+    setRecordDetail({
+      ...recordDetail,
+      insulineU: value.target.value
+    });
+  }
+
+  useEffect(() => {
+    Unit.find('glyc', {}, true).then((glycemiaUnits) => {
+      if(glycemiaUnits == null) {
+        setGlycemiaUEnum([]);
+      }
+      else {
+        setGlycemiaUEnum(glycemiaUnits);
+      }
+    })
+  }, []);
+
+  const [insulineTEnum, setInsulineTEnum] = useState([]);
+
+  useEffect(() => {
+    Unit.find('insuline', {}, true).then(insulineTypes => {
+      if(insulineTypes == null) {
+        setInsulineTEnum([]);
+      }
+      else {
+        setInsulineTEnum(insulineTypes);
+      }
+    })
+  }, []);
+
+
   const [expanded, setExpanded] = React.useState(true);
 
   const handlePress = () => setExpanded(!expanded);
@@ -20,65 +83,11 @@ export default function EntryDetail({ navigation }) {
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
   const [isTimePickerVisible, setTimePickerVisibility] = useState(false);
 
-  const onSave = () => {
-      Vibration.vibrate([1000]);
-  }
-
-  const onRetrieve = () => {
-      
-  }
-
-  const inputChange = text => {
-      
-  }
-
-  const getDateFormatted = () => {
-      let year = dateTime.getFullYear();
-      let day = dateTime.getDate();
-      let month = dateTime.getMonth();
-
-      return `${day}. ${month}. ${year}`; //Day. Month. Year
-  }
-
-  const getTimeFormatted = () => {
-      let hours = dateTime.getHours();
-      let minutes = dateTime.getMinutes();
-
-      return `${hours}:${minutes < 10 ? '0' : ''}${minutes}`; //Hours:Minutes
-  }
-
-  const showDatePicker = () => {
-      setDatePickerVisibility(true);
-  }
-
-  const hideDatePicker = () => {
-      setDatePickerVisibility(false);
-  }
-
-  const showTimePicker = () => {
-      setTimePickerVisibility(true);
-  }
-
-  const hideTimePicker = () => {
-      setTimePickerVisibility(false);
-  }
-
-  const dateSelectionConfirm = (date) => {
-      hideDatePicker();
-
-      setDateTime(date);
-  };
-
   const timeSelectionConfirm = (time) => {
       hideTimePicker();
 
       setDateTime(time);
   };
-
-    //let props=[{record: records}];
-    //load records from storage
-    let i = 0;
-    let cont = true;
 
     const styles = StyleSheet.create({
       list_flex: {
@@ -126,70 +135,58 @@ export default function EntryDetail({ navigation }) {
         <Text style={styles.text_style.normal}>Hladina cukru</Text>
         <View style={styles.list_flex}>
 
-          <InputSpinner 
-          style={{maxWidth: '100%'}}
-          rounded= {false}
-          showBorder={true}
-          placeholder="Nezadáno"
-          precision={1}
-          type="real"
-          emptied={true}
-          min={0}
-          max={100}
-          step={0.1}
-          color={primaryColor}
-          fontSize={26}
-          append={
-            <ModalDropdown
-              textStyle={{fontSize: 20}} // text style of the dropdown
-
-              dropdownTextStyle={{fontSize: 20, margin: 10, textAlign: 'center'}} // text style of the dropdown options
-              dropdownStyle={{margin: -12, height: 140}} // style of the dropdown
-
-              options={['mmol/l', 'mg/dl']} // options to choose from
-              style={{margin: 10}} // style of the dropdown
-              defaultValue="Jednotky" // default text of the dropdown
-              isFullWidth={true}
-              renderRightComponent={() => <MaterialCommunityIcons name="chevron-down" size={24} color="black"/>} // Dropdown icon
-
-            />} // Appended element
+        <InputSpinner 
+            rounded= {false}
+            showBorder={true}
+            precision={1}
+            placeholderTextColor={placeholderColor}
+            placeholder="N"
+            type="real"
+            emptied={true}
+            min={0}
+            step={0.1}
+            max={100}
+            color={primaryColor}
+            value={recordDetail.bloodSugar}
+            onChange={updatedBloodSugar}
+            fontSize={ 28 }
+            append={
+              <AppendDropdown
+                data={glycemiaUEnum}
+                value={recordDetail.bloodSugarU}
+                onChange={updatedBloodSugarU}
+              ></AppendDropdown>
+            } // Appended element
           />
         </View>
         
-        <Text style={styles.text_style.normal}>Podaný inzulín</Text>
+        <Text style={styles.text_style.normal}>Inzulín (jednotky)</Text>
         <View style={styles.list_flex}>
+          
           <InputSpinner 
-          rounded= {false} // makes the spinner not round duh
-          showBorder={true} // shows the border of the spinner
-          emptied={true} // if input can be empy, not sure if it works the way I think it does
-          precision={1} // how many decimals can be inputted
-          type="real" // type of input, gets the decimal point
-          max={9999999}
-          placeholder="Nezadáno" // placeholder text when empty
-          min={0} // minimum value
-          step={0.1} // step size of the number
-          color={primaryColor} // color of the spinner
-          fontSize={26} // text size of the spinner
-
-          append={ // Appended element, right side of the spinner
-            <ModalDropdown
-              style={{margin: 10}} // style of the dropdown
-              textStyle={{fontSize: 20}} // text style of the dropdown
-
-              dropdownTextStyle={{fontSize: 20, margin: 10, textAlign: 'center'}} // text style of the dropdown options
-              dropdownStyle={{margin: -12, height: 140}} // style of the dropdown options
-
-              options={['Fiasp', 'option 2']} // options to choose from
-              defaultValue="Jednotky" // default text of the dropdown
-              isFullWidth={true} // should make the dropdown options be the same width as the dropdown but it doesn't work
-
-              renderRightComponent={() => <MaterialCommunityIcons name="chevron-down" size={24} color="black"/>} // Dropdown icon
-
-            />} // Appended element
-          /> 
+            rounded= {false}
+            showBorder={true}
+            placeholder="N"
+            placeholderTextColor={placeholderColor}
+            precision={1}
+            type="real"
+            emptied={true}
+            min={0}
+            step={1}
+            color={primaryColor}
+            value={recordDetail.insuline}
+            onChange={updatedInsuline}
+            fontSize={ 28 }
+            append={
+            <AppendDropdown
+              data={insulineTEnum}
+              value={recordDetail.insulineT}
+              onChange={updatedInsulineT}
+            ></AppendDropdown>
+            }
+          />
         </View>
 
-        
         <View style={styles.timeinputcontainer}>
           <DateTimePickerWithText
             value={dateTime}
