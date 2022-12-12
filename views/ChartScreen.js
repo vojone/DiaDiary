41682a2c5413156ca80dd3a59abe7cbfcee8c8c9
Juraj Dedič <jurajdedic@gmail.com>
2022-12-12 19:58:11 +0100

    visual overhaul of chart screen

--- views/ChartScreen.js
+++ views/ChartScreen.js
@@ -1,8 +1,16 @@
+/**
+ * Screen for displaying chart of blood sugar values
+ * Author:  Juraj Dedič (xdedic07)
+ */
+
 import { Record } from "../models/record";
 
 
 import { Dimensions, StyleSheet, Text, View, Button } from "react-native";
-import React, { useState, useEffect } from "react";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import React, { useState, useEffect, useRef, useMemo, useCallback } from "react";
+
+import { DefaultTheme } from "react-native-paper";
 
 import { LineChart, AreaChart, Grid, YAxis, XAxis } from 'react-native-svg-charts'
 import { Path, Circle, Defs, LinearGradient, Stop } from 'react-native-svg'
@@ -10,6 +18,8 @@ import * as shape from 'd3-shape'
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import { func } from "prop-types";
 // import { Dots, Line } from '@/screens/AreaChartScreen/chartAdds'
+
+import BottomSheet from '@gorhom/bottom-sheet';
 
 function getDaysBack(date,n = 1){
     let d = new Date(date)
@@ -25,7 +35,7 @@ function getDisplayDate(date) {
   if (date.getDate() == today.getDate() && date.getMonth() == today.getMonth() && date.getFullYear() == today.getFullYear()){
     return date.getHours() + ":" + date.getMinutes();
   }
-  return date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear();
+  return date.getDate() + "." + (date.getMonth() + 1 + ".");
 }
 
 
@@ -156,7 +166,7 @@ function getMax(data){
   return max;
 }
 
-export default function ChartScreen(props) {
+export default function ChartScreen({ navigation }) {
 
   const [dateFrom, setDateFrom] = useState(null);
   const [dateTo, setDateTo] = useState(null);
@@ -220,6 +230,10 @@ export default function ChartScreen(props) {
       }
       let data = result.sort((a, b) => a["dateTime"] - b["dateTime"]);
       setData(data);
+      let lastData = data[data.length - 1];
+      if(lastData != null){
+        setLastUpdate(lastData);
+      }
     });
   },[dateFrom, dateTo]);
   
@@ -236,8 +250,170 @@ export default function ChartScreen(props) {
 
   console.log("data", data[0]);
 
+  //open the bottom sheet
+  const openSheet = () => {
+    
+  };
+
+   // ref
+   const bottomSheetRef = useRef(null);
+
+   // variables
+   const snapPoints = useMemo(() => ['40%', '45%'], []);
+
+   // callbacks
+  const handleSheetChanges = useCallback((index) => {}, []);
+
+  function optionsClick(){
+    bottomSheetRef.current.expand();
+  }
+
+  useEffect(() => {
+    bottomSheetRef.current.close();
+  }, []);
+
+   const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 24,
+      backgroundColor: 'grey',
+    },
+    contentContainer: {
+      flex: 1,
+      alignItems: 'center',
+    },
+  });
+ 
+
     return (
-        <View>
+  <View style={{
+    backgroundColor: "#5924EB"
+  }}>
+      <Text style={{
+        color: "white",
+        fontSize: 30,
+        fontWeight: "normal",
+        marginTop: 30,
+        marginHorizontal: 20,
+      }}>Poslední záznam hladiny cukru v krvi: { lastUpdate && lastUpdate["bloodSugarU"] ? lastUpdate["bloodSugar"]+" "+lastUpdate["bloodSugarU"]["label"] : "neznámá" }</Text>
+      <Text style={{
+        color: "white",
+        fontSize: 20,
+        fontWeight: "normal",
+        marginTop: 10,
+        marginHorizontal: 20,
+      }}>Aktualizace: { lastUpdate ? getDisplayDate(lastUpdate["dateTime"]) : "neznámo" }</Text>
+    <View style={{
+      height: "100%",
+      backgroundColor: "#f5f5f5",
+      marginTop: 20,
+      paddingTop: 20,
+      borderTopLeftRadius: 30,
+      borderTopRightRadius: 30,
+    }}>
+
+      <Text style={{ fontSize: 24, fontWeight: "bold", marginHorizontal: 20, marginBottom: 10 }}>Záznamy</Text>
+
+      <DateTimePickerModal
+          isVisible={isFromDatePickerVisible}
+          mode="date"
+          onConfirm={handleFromConfirm}
+          onCancel={hideFromDatePicker}
+      />
+
+      <DateTimePickerModal
+          isVisible={isToDatePickerVisible}
+          mode="date"
+          onConfirm={handleToConfirm}
+          onCancel={hideToDatePicker}
+      />
+    
+      <View style={{
+        backgroundColor: "white",
+        borderRadius: 30,
+        padding: 20,
+        marginHorizontal: 10,
+      }}>
+        <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center", width: "100%" }}>
+        <Text 
+          style={{
+            fontSize: 24,
+            marginBottom: 0,
+            fontWeight: "bold"
+          }}>
+            Přehled v grafu
+        </Text>
+        <MaterialCommunityIcons name="dots-vertical" size={24} color="black" onPress={optionsClick} />
+        </View>
+        <View style={{
+          flexDirection: "row",
+          marginVertical: 10,
+          position: "relative",
+          height: 210,
+          }}>
+          <YAxis
+            data={data}
+            contentInset={{ top: 20, bottom: 20 }}
+            min={getMin(data)}
+            max={getMax(data)}
+            svg={{
+              fill: 'grey',
+              fontSize: 11,
+            }}
+            style={{ marginRight: 0 }}
+            formatLabel={(value) => `${value} mmol/L`}
+            numberOfTicks={6}
+          />
+          <View style={{ flex: 1, marginLeft: 10 }}>
+            <LineChart
+              style={{ width: "100%", height: "100%" }}
+              data={getDisplayData(data)}
+              contentInset={{ top: 20, bottom: 20 }}
+              curve={shape.curveNatural}
+              // svg={{ fill: 'rgba(134, 65, 244, 0.8)' }}
+              svg={{
+                stroke: 'url(#gradient)',
+              }}
+            >
+              <Line/>
+              <Dots/>
+              <Gradient/>
+              <Shadow/>
+              <Grid/>
+            </LineChart>
+            <XAxis
+              style={{ marginHorizontal: -10 }}
+              data={data}
+              formatLabel={(value, index) => {
+                if (index % 4 === 0) {
+                  console.log("value", value, "index", index);
+                  let date = new Date(data[index]["dateTime"]);
+                  return getDisplayDate(date)
+                }
+                return ''
+              }}
+              contentInset={{ left: 10, right: 10 }}
+              svg={{ fontSize: 10, fill: 'black' }}
+            />
+          </View>
+        </View>
+      </View>
+
+      <View style={{ justifyContent: "space-between", flexDirection: "row", alignItems: "center", marginHorizontal: 20, marginTop: 20 }}>
+        <Text style={{ fontSize: 24, fontWeight: "bold", marginHorizontal: 10, marginBottom: 10 }}>Seznam záznamů</Text>
+        <MaterialCommunityIcons name="arrow-right" size={24} color="black" onPress={() => navigation.navigate("Historie")} />
+      </View>
+    </View>
+
+    <BottomSheet
+      ref={bottomSheetRef}
+      index={1}
+      snapPoints={snapPoints}
+      onChange={handleSheetChanges}
+      enablePanDownToClose={true}
+    >
+      <View style={styles.contentContainer}>
+        <Text style={{marginBottom: 10}}>Nastavení grafu 🎉</Text>
 
         <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center", width: "100%", paddingLeft: 20, paddingRight: 20, paddingBottom: 10}}>
           <Text>Datum od: {getDisplayDate(dateFrom)}</Text>
@@ -247,7 +423,7 @@ export default function ChartScreen(props) {
           </View>
         </View>
 
-         <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center", width: "100%", paddingLeft: 20, paddingRight: 20, paddingBottom: 10}}>
+        <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center", width: "100%", paddingLeft: 20, paddingRight: 20, paddingBottom: 10}}>
           <Text>Datum do: {getDisplayDate(dateTo)}</Text>
           <View style={{flexDirection: "row"}}>
               <Button title="Změnit" onPress={showToDatePicker} />
@@ -255,73 +431,8 @@ export default function ChartScreen(props) {
           </View>
         </View>
 
-        <DateTimePickerModal
-            isVisible={isFromDatePickerVisible}
-            mode="date"
-            onConfirm={handleFromConfirm}
-            onCancel={hideFromDatePicker}
-        />
-
-        <DateTimePickerModal
-            isVisible={isToDatePickerVisible}
-            mode="date"
-            onConfirm={handleToConfirm}
-            onCancel={hideToDatePicker}
-        />
-       
-    <Text>Bezier Line Chart</Text>
-    
-    <View style={{
-      flexDirection: "row",
-      marginVertical: 10,
-      position: "relative",
-      height: 200}}>
-       <YAxis
-        data={data}
-        contentInset={{ top: 20, bottom: 20 }}
-        min={getMin(data)}
-        max={getMax(data)}
-        svg={{
-          fill: 'grey',
-          fontSize: 11,
-        }}
-        style={{ marginRight: 5 }}
-        formatLabel={(value) => `${value} mmol/L`}
-        numberOfTicks={6}
-      />
-      <View style={{ flex: 1, marginLeft: 10 }}>
-        <LineChart
-          style={{ width: "100%", height: "100%" }}
-          data={getDisplayData(data)}
-          contentInset={{ top: 20, bottom: 20 }}
-          curve={shape.curveNatural}
-          // svg={{ fill: 'rgba(134, 65, 244, 0.8)' }}
-          svg={{
-            stroke: 'url(#gradient)',
-          }}
-        >
-          <Line/>
-          <Dots/>
-          <Gradient/>
-          <Shadow/>
-          <Grid />
-        </LineChart>
-        <XAxis
-          style={{ marginHorizontal: -10 }}
-          data={data}
-          formatLabel={(value, index) => {
-            if (index % 4 === 0) {
-              console.log("value", value, "index", index);
-              let date = new Date(data[index]["dateTime"]);
-              return getDisplayDate(date)
-            }
-            return ''
-          }}
-          contentInset={{ left: 10, right: 10 }}
-          svg={{ fontSize: 10, fill: 'black' }}
-        />
       </View>
-    </View>
-    </View>
+    </BottomSheet>
+  </View>
     )
 }
